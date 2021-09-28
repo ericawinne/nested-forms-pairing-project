1ab6704645efbfda809cce2d9e96464096582b56
Erica Winne <erica.winne@powerhrg.com>
2021-09-28 14:27:15 -0400

    pairing done

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -9,8 +9,19 @@ class DoctorsController < ApplicationController
 
   def new
     @doctor = Doctor.new
+    @doctor.patients.build(name: "Patients Name")
+    @doctor.patients.build(name: "Patients Name")
   end
 
   def create
+    Doctor.create(doctor_params)
+    redirect_to doctors_path
+  end
+
+  def doctor_params
+    params.require(:doctor).permit(
+      :name,
+      patients_attributes: [ :name, :age ]
+    )
   end
 end
